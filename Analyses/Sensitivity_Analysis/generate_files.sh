0fb13464c12147e4a22a38e76cf9efd4bb387c3e
Igor Marin de Mas <41136363+igormdem@users.noreply.github.com>
2024-05-03 06:37:53 +0200

    Add files via upload

---
+++ Analyses/Sensitivity_Analysis/generate_files.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Define the number of samples
+n=95
+
+# Loop over the number of samples
+for ((i=1;i<=n;i++)); do
+    # Create a new .m file with the sample number replaced
+    sed "s/sample_n = 1;/sample_n = $i;/" sensitivity_analysis.m > sensitivity_analysis_$i.m
+
+    # Create a new .sh file with the sample number replaced      
+    #echo "#!/bin/bash" > sensitivity_analysis_$i.sh
+    #echo "nohup matlab -nojvm -r 'sensitivity_analysis_$i;exit'" >> sensitivity_analysis_$i.sh
+
+    # Make the new .sh file executable
+    #chmod +x sensitivity_analysis_$i.sh
+done
+
